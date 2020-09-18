2635ff5e00238ab02a81cf0227460fa9989086d1
Vasudev Gupta <7vasudevgupta@gmail.com>
2020-09-19 04:53:14 +0530

    Notebook, lab instructions added

--- terminal-command-line/conda-env-spyder-setup.sh
+++ terminal-command-line/conda-env-spyder-setup.sh
@@ -1,20 +1,20 @@
 # INSTALL MINICONDA
-# run the script at https://docs.conda.io/en/latest/miniconda.html using shell
+# run the script at "https://docs.conda.io/en/latest/miniconda.html" using shell
 
 # create your virtual env if you wish
 conda create -n insights
 conda activate insights
 conda init
 
+# setup jupyter-notebook
+conda install -c conda-forge notebook
+
+# setup jupyter-lab
+conda install -c conda-forge jupyterlab
+
 # setup spyder
 pip install spyder
-
 conda install spyder-terminal -c spyder-ide
-
 pip install spyder-kernels
-
-# add requirements
-#pip install -r requirements.txt
-
 ## RUN BELOW COMMAND IN TERMINAL AND YOU ARE GOOD TO LAUNCH SPYDER. YAYYY
 # spyder --reset
